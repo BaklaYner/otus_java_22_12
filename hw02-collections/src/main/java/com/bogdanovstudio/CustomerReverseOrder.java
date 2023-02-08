a8c52a00497d351567539274174c3834ea3a112d
BogdanovAA <bogdanov.ahtoh@gmail.com>
2023-02-08 15:22:02 +0300

    Implementing of CustomerReverseOrder class for reverseOrderTest()

--- hw02-collections/src/main/java/com/bogdanovstudio/CustomerReverseOrder.java
+++ hw02-collections/src/main/java/com/bogdanovstudio/CustomerReverseOrder.java
@@ -1,14 +1,15 @@
 package com.bogdanovstudio;
 
+import java.util.LinkedList;
+
 public class CustomerReverseOrder {
-    //todo: 2. надо реализовать методы этого класса
-    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final LinkedList<Customer> customersStack = new LinkedList<>();
 
     public void add(Customer customer) {
-
+        customersStack.push(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return customersStack.pop();
     }
 }
