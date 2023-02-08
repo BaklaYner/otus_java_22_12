1cf4685d09105bf7dff169133bea5cef8f52c24a
BogdanovAA <bogdanov.ahtoh@gmail.com>
2023-02-08 15:44:10 +0300

    remove @Disabled annotation from scoreSortingTest() and mutationTest(). Remove redundant comment and import

--- hw02-collections/src/test/java/com/bogdanovstudio/CustomerTest.java
+++ hw02-collections/src/test/java/com/bogdanovstudio/CustomerTest.java
@@ -1,6 +1,5 @@
 package com.bogdanovstudio;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +9,6 @@ import java.util.Map;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CustomerTest {
-    // Все тесты должны проходить, менять тесты не надо.
-
     @Test
     @DisplayName("Check that Customer class is not broken")
     void setterCustomerTest() {
@@ -55,7 +52,6 @@ public class CustomerTest {
     }
 
     @Test
-    @Disabled //надо удалить
     @DisplayName("Sort by score field value asc")
     void scoreSortingTest() {
         //given
@@ -95,7 +91,6 @@ public class CustomerTest {
     }
 
     @Test
-    @Disabled //надо удалить
     @DisplayName("Collection modification")
     void mutationTest() {
         //given
